474bf7645e6e8e2c0517f97de5092c292062a858
Kielta1 <92026049+Kielta1@users.noreply.github.com>
2024-05-28 15:12:28 +0200

    Add files via upload

---
+++ MovieDatabase/Controllers/MovieSceneController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieDatabase.Controllers
+{
+    public class MovieSceneController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieDatabase/Controllers/MovieSceneController2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieDatabase.Controllers
+{
+    public class MovieSceneController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
