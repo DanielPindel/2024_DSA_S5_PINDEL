c7deaf0adacd5b064029f4391621e55c75066675
Byouts <mo300354@student.polsl.pl>
2024-06-17 21:45:42 +0200

    Profile statistics

--- MovieDatabase/Controllers/UserProfileController.cs
+++ MovieDatabase/Controllers/UserProfileController.cs
@@ -13,8 +13,58 @@ namespace MovieDatabase.Controllers
 {
     public class UserProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly MovieDatabaseContext _context;
+
+        // Watchlist constructor that assigns our context to the class
+        public UserProfileController(MovieDatabaseContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string? id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var user = await _context.User.FirstOrDefaultAsync(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userMovies = _context.UserMovie
+                       .Where(um => um.user_id == id)
+                       .ToList();
+            var ratings = _context.Rating
+                       .Where(r => r.user_id == id)
+                       .ToList(); ;
+
+            int watchlistNo = 0;
+            int favNo = 0;
+            int ratedNo = 0;
+
+            foreach (var userMovie in userMovies)
+            {
+                if (userMovie.context_id == 1)
+                {
+                    watchlistNo++;
+                }
+                if (userMovie.context_id == 2)
+                {
+                    favNo++;
+                }
+            }
+            foreach(var rating in ratings)
+            {
+                ratedNo++;
+            }
+
+            ViewBag.watchlistNoVB = watchlistNo;
+            ViewBag.favNoVB = favNo;
+            ViewBag.ratedNoVB = ratedNo;
+
             return View();
         }
     }
